3ce676e52b037c9a0ed4de112291c3e934aa2452
Fran Ramirez <franronalramirezvaldez06@gmail.com>
2026-04-13 18:53:30 -0500

    subiendo cambios reales

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO estado_asistencia (nombre) VALUES ('Presente');
+INSERT INTO estado_asistencia (nombre) VALUES ('Tarde');
+INSERT INTO estado_asistencia (nombre) VALUES ('Falta');
+INSERT INTO estado_asistencia (nombre) VALUES ('Justificado');
